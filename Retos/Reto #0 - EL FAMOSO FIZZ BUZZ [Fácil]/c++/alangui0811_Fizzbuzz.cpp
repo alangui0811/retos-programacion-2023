d46ea0e3d8c73be18af0ab28f87cc6221c276fd2
alangui0811 <115850212+alangui0811@users.noreply.github.com>
2023-05-26 13:32:14 -0600

    Add files via upload

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c++/alangui0811_Fizzbuzz.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    system("clear");
+    for (int i=1;i<100;i++){
+        if(i%3==0 && i%5==0){
+            cout<<"fizzbuzz "<<endl;
+        }
+        else if (i%3==0)
+        {
+            cout<<"fizz "<<endl;
+        }
+        else if(i%5==0){
+            cout<<"buzz "<<endl;
+        }
+        else{
+            cout<<i<<endl;
+        }
+    }
+
+    return 0;
+}
